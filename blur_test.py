e9d29bd96964e80abb792bb28544c02042aba8c6
grakesh0301@gmail.com <Rakesh1997>
2020-07-11 09:47:19 +1000

    added fer library for emotion detection

--- blur_test.py
+++ blur_test.py
@@ -4,8 +4,9 @@ import numpy as np
 from shapely.geometry import Polygon
 import mouth_open
 from PIL import Image
+from fer import FER
 
-img = cv2.imread("download.png")
+img = cv2.imread("smile1.jpg")
 a, b, c = img.shape
 
 face_locations = face_recognition.face_locations(img)
@@ -29,6 +30,10 @@ def calculate_iou(box_1, box_2):
 box_1 = [[list[0][3], list[0][0]], [list[0][1], list[0][0]], [list[0][1], list[0][2]], [list[0][3], list[0][2]]]
 box_2 = [[0, 0], [a, 0], [a, b], [0, b]]
 
+#emotions
+detector = FER()
+emotion, score = detector.top_emotion(img)
+
 iou = calculate_iou(box_1, box_2)
 iou = round(iou*100)
 print(iou)
@@ -45,6 +50,7 @@ out = np.where(mask==np.array([255, 255, 255]), img, blurred_img)
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(out, str(iou)+'%', (5,50), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
 cv2.putText(out, "Is teeth visible: "+ str(teeth), (5,70), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
+cv2.putText(out, emotion+":"+ str(round(score*100)) + "%", (5,110), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.imshow("image", out)
